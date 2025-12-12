b53667e65828ad81ab2c8423abd8588f835c9c0d
Corentin Delvaque <corentin.delvaque@icloud.com>
2025-12-12 10:38:39 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  base: "/Initialisation_Tailwind",
+});
+
+//npm install
+
+//extension prettier + format
+
+//extension Tailwind CSS IntelliSense dans VSCode
+
+//npm run dev
